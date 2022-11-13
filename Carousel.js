e03b956b7554bab446073de57ebd2e52f87d7af4
Nurudeen Ajadi <114497063+one-life-1@users.noreply.github.com>
2022-11-13 14:46:19 +0000

    Create Carousel.js

---
+++ Carousel.js
@@ -0,0 +1,16 @@
+
+  <script>
+    const swiper = new Swiper(".swiper", {
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+  </script>
